4e2368a4c2605ed800eafd749f117b4157a697f7
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 12:24:51 -0300

    EjercicioI

---
+++ CAI_EjercicioI/CAI_EjercicioI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioI
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int registro = 691;
+
+            for (int i = 1; i <= registro; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " - FooBar");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " - Foo");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " - Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
